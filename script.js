8fdb5cecbd70e4da9d9fba5cea9d3e6fa8a99844
Bhavith.S <bhaviths20@gmail.com>
2024-12-07 21:23:27 +0530

    Added logo and redesigned the nav bar.

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('click',()=>{
+    if(sessionStorage.getItem('hasAccount')){
+        sessionStorage.removeItem('hasAccount')
+    } else {
+        sessionStorage.setItem('hasAccount', true)
+    }
+})
+
+if(!sessionStorage.getItem('hasAccount')){
+    document.getElementById('profile').style.display = "block";
+    showMenu()
+    document.getElementById('signup-login').style.display = "none";
+} else {
+    document.getElementById('signup-login').style.display = "flex";
+    document.getElementById('menu-drop-down').style.display = "none";
+    document.getElementById('profile').style.display = "none";
+}
+
+function showMenu() {
+    let menu = Array.from(document.getElementsByClassName('menu'))[0];
+    let menuHolder = document.getElementById('menu-holder')
+    document.getElementById('profile').addEventListener('mouseenter',()=>{
+        menu.classList.add('menu-drop-down');
+        menuHolder.style.display = "block";
+    })
+    document.getElementById('profile').addEventListener('mouseleave',()=>{
+        menu.classList.remove('menu-drop-down');
+        menuHolder.style.display = "none";
+    })
+
+    menuHolder.addEventListener('mouseenter',()=>{
+        menu.classList.add('menu-drop-down');
+        menuHolder.style.display = "block";
+    })
+
+    menuHolder.addEventListener('mouseleave',()=>{
+        menu.classList.remove('menu-drop-down');
+        menuHolder.style.display = "none";
+    })
+}
